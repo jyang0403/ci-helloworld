9592a38c341f76c79d701d4dbd67b8490abf797e
Jiashu Yang <jiashuyang0403@gmail.com>
2024-03-27 17:00:01 -0400

    upload

--- src/test/java/ArrayUtilsTest.java
+++ src/test/java/ArrayUtilsTest.java
@@ -45,4 +45,19 @@ public class ArrayUtilsTest {
     public void testOddOrPositiveBothPositivesAndNegatives() {
         assertEquals(3, ArrayUtils.oddOrPos(new int[]{-3, -2, 0, 1, 4}));
     }
+
+    @Test
+    public void testCountOfEmpty() {
+        assertEquals(0, ArrayUtils.countOf(new int[]{}, 0));
+    }
+
+    @Test
+    public void testCountOfNotFound() {
+        assertEquals(0, ArrayUtils.countOf(new int[]{1, 2, 3, 4, 5}, 0));
+    }
+
+    @Test
+    public void testCountOfFound() {
+        assertEquals(3, ArrayUtils.countOf(new int[]{1, 2, 3, 4, 5, 1, 1}, 1));
+    }
 }
